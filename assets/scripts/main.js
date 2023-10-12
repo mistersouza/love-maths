1e5c3d63accf64355271c3e7b7eaec3e527bb07e
mistersouza <>
2023-10-12 08:42:10 +0000

    Add event listeners for document and buttons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,22 @@
-function runGame(params) { }
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.querySelectorAll('button');
 
-function checkAnswer(params) { }
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            if (this.getAttribute('data-type' == 'submit')) {
+                console.log('You\ve clicked submit!');
+                return;
+            }
+            let gameType = this.getAttribute('data-type');
+            alert(`You've clicked ${gameType}`);
+
+        });
+    }
+});
+
+function runGame() { }
+
+function checkAnswer() { }
 
 function calculateCorrectAnswer() { }
 
