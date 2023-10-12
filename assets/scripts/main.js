bcbdaf49964d9263b122959319450108d60d4465
mistersouza <>
2023-10-12 08:09:36 +0000

    Lay out functions needed

---
+++ assets/scripts/main.js
@@ -0,0 +1,22 @@
+function runGame(params) { }
+
+function checkAnswer(params) { }
+
+function calculateCorrectAnswer() { }
+
+function incrementScore() { }
+
+function incrementWrongAnswer() { }
+
+function displayAddtion() { }
+
+function displayDivision() { }
+
+function displayMultiplication() { }
+
+function displaySubtraction() { }
+
+
+
+
+
